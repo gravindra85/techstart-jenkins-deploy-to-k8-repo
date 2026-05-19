7513c8682e1c143bb1e87009967a2a8df045e029
Ravindra Gollapelli <gravindra85@gmail.com>
2026-05-19 19:03:19 +0530

    syntax corrections

--- src/main/java/org/t246osslab/easybuggy/exceptions/MalformedParameterizedTypeExceptionServlet.java
+++ src/main/java/org/t246osslab/easybuggy/exceptions/MalformedParameterizedTypeExceptionServlet.java
@@ -1,6 +1,7 @@
 package org.t246osslab.easybuggy.exceptions;
 
 import java.io.IOException;
+import java.lang.reflect.MalformedParameterizedTypeException;
 import java.lang.reflect.Type;
 import java.util.List;
 
@@ -11,14 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.t246osslab.easybuggy.core.servlets.AbstractServlet;
 
-import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
-
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = { "/mpte" })
 public class MalformedParameterizedTypeExceptionServlet extends AbstractServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        ParameterizedTypeImpl.make(List.class, new Type[]{}, null);
+        throw new ServletException(new MalformedParameterizedTypeException());
     }
 }
